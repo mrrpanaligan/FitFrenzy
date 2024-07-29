20b0c7218cc0d40942f6f74a79c7052fe51dba01
Marc Panaligan <mrrpanaligan@gmail.com>
2024-07-29 16:30:14 +0800

    Initial Commit

---
+++ FitFrenzy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitFrenzy
+//
+//  Created by Marc Panaligan on 7/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitFrenzy/FitFrenzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFrenzyApp.swift
+//  FitFrenzy
+//
+//  Created by Marc Panaligan on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FitFrenzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
